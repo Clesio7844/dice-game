c0dbc9868fce87129c0d48a83f5dac02f3231ff7
David De Castro <daviddecastro42@live.co.uk>
2019-10-24 15:07:34 +0100

    update second dice but button is cnot work

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 //document.querySelector("h1").innerHTML = "player game";
 let list = Math.floor(Math.random() * 6) + 1;
+let secondlist = Math.floor(Math.random() * 6) + 1;
 let playergameimage = "image/dice" + list +".png";
+let newagmelist = "image/dice" + secondlist +".png";
+const bnutton = document.getElementById("button")
 
 document.querySelector(".img1").setAttribute("src", playergameimage);
+document.querySelector(".img2").setAttribute("src", newagmelist);
 
-if(playergameimage > 6){
-    document.querySelector("h1").innerHTML = ""
-}
+
+    if(playergameimage > newagmelist){
+        document.querySelector("h1").innerHTML = " Player 1 Win"
+    }else if(playergameimage < newagmelist){
+        document.querySelector("h1").innerHTML = " Player 2 Win"
+    }else {
+        document.querySelector("h1").innerHTML = " Roll Again"
+    }
+
+
+
+// if(playergameimage > 6){
+//     document.querySelector("h1").innerHTML = ""
+// }
